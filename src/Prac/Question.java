35ce8454878dc800d00b9745d6e0a46586da98ec
sclee <sclee9961@gmail.com>
2025-09-24 14:46:44 +0900

    커밋

--- src/Prac/Question.java
+++ src/Prac/Question.java
@@ -1,5 +1,17 @@
 package Prac;
 
-public class Question {
+import java.util.Scanner;
 
+public class Question {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println(("정수를 입력하시오:"));
+        int n = sc.nextInt();
+
+        if (n % 2 == 0){
+            System.out.println("짝수 :");
+        } else {
+            System.out.println("홀수 :");
+        }
+    }
 }
